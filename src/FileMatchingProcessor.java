3ff49b2623319134d6c4025bbf9be72d16340681
sainithish <venkata.gsnc@gmail.com>
2024-06-30 20:19:37 -0700

    Second push-5

--- src/FileMatchingProcessor.java
+++ src/FileMatchingProcessor.java
@@ -60,7 +60,7 @@ public class FileMatchingProcessor implements Runnable {
 
         for (String word : words) {
             //Changing to lower case for my simplicity
-            if (uniqueWords.contains(line.toLowerCase())) {
+            if (uniqueWords.contains(word.toLowerCase())) {
                 //Obtaining lock to update matchStringBuilder
                 reentrantLock.lock();
                 try {
